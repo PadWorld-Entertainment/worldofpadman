4bce0a5e98a999de58ded37f9c7c232a22f96705
Zack Middleton <zturtleman@gmail.com>
2013-09-03 18:19:48 -0500

    Make cgame's tcmd use trap_SendClientCommand for "gc"
    gc command was forwarded to server after if wasn't recognized locally, but let's just send straight to server.
    
    (trap_SendConsoleCommand should of had a \n at end, but using trap_SendClientCommand makes more sense.)

--- code/cgame/cg_consolecmds.c
+++ code/cgame/cg_consolecmds.c
@@ -35,7 +35,7 @@ static void CG_TargetCommand_f(void) {
 	}
 
 	trap_Argv(1, test, 4);
-	trap_SendConsoleCommand(va("gc %i %i", targetNum, atoi(test)));
+	trap_SendClientCommand(va("gc %i %i", targetNum, atoi(test)));
 }
 
 /*
