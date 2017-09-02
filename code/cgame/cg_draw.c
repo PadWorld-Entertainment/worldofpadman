523cf4883749d10cd8ce2720fd0c045c96234492
Zack Middleton <zack@cloemail.com>
2017-09-02 17:57:24 -0500

    Fix team chat box for spectators
    Make spectators use green background for team chat box when following
    players. The team chat messages are from spectators not the team of
    the followed player.
    
    Make spectators draw team chat box even when not following a player.

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -1133,12 +1133,12 @@ static void CG_DrawTeamInfo(void) {
 
 		h = (cgs.teamChatPos - cgs.teamLastChatPos) * TINYCHAR_HEIGHT;
 
-		if (cg.snap->ps.persistant[PERS_TEAM] == TEAM_RED) {
+		if (cgs.clientinfo[cg.clientNum].team == TEAM_RED) {
 			hcolor[0] = 1.0f;
 			hcolor[1] = 0.0f;
 			hcolor[2] = 0.0f;
 			hcolor[3] = 0.33f;
-		} else if (cg.snap->ps.persistant[PERS_TEAM] == TEAM_BLUE) {
+		} else if (cgs.clientinfo[cg.clientNum].team == TEAM_BLUE) {
 			hcolor[0] = 0.0f;
 			hcolor[1] = 0.0f;
 			hcolor[2] = 1.0f;
