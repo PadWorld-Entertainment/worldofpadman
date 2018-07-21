29a866aa9dee6942429652e47a2108b2ec2fb6cb
Zack Middleton <zack@cloemail.com>
2018-07-20 23:40:39 -0500

    OpenGL1: Use vertex arrays instead of glBegin
    This is loosely based on the OpenPandora OpenGL ES port by ptitSeb.
    Some of the changes were backported from the opengl2 renderer.
    
    It runs faster, at least on modern hardware, and is requires for
    OpenGL ES support.
    
    r_primatives cvar values 1 and 3 still use glBegin as they have to use
    it by definition.

--- code/renderergl1/tr_backend.c
+++ code/renderergl1/tr_backend.c
@@ -646,6 +646,46 @@ static void RB_SetGL2D(void) {
 }
 
 /*
+================
+RB_InstantQuad2
+================
+*/
+void RB_InstantQuad2(vec4_t quadVerts[4], vec2_t texCoords[4]) {
+	glIndex_t indexes[6];
+
+	qglDisableClientState(GL_COLOR_ARRAY);
+	qglEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	qglTexCoordPointer(2, GL_FLOAT, 0, texCoords);
+	qglVertexPointer(3, GL_FLOAT, 16, quadVerts);
+
+	indexes[0] = 0;
+	indexes[1] = 1;
+	indexes[2] = 2;
+	indexes[3] = 0;
+	indexes[4] = 2;
+	indexes[5] = 3;
+
+	R_DrawElements(6, indexes);
+}
+
+/*
+================
+RB_InstantQuad
+================
+*/
+void RB_InstantQuad(vec4_t quadVerts[4]) {
+	vec2_t texCoords[4] = {
+		{0, 0},
+		{1, 0},
+		{1, 1},
+		{0, 1}
+	};
+
+	RB_InstantQuad2(quadVerts, texCoords);
+}
+
+/*
 =============
 RE_StretchRaw
 
@@ -696,16 +736,22 @@ void RE_StretchRaw(int x, int y, int w, int h, int cols, int rows, const byte *d
 
 	qglColor3f(tr.identityLight, tr.identityLight, tr.identityLight);
 
-	qglBegin(GL_QUADS);
-	qglTexCoord2f(0.5f / cols, 0.5f / rows);
-	qglVertex2f(x, y);
-	qglTexCoord2f((cols - 0.5f) / cols, 0.5f / rows);
-	qglVertex2f(x + w, y);
-	qglTexCoord2f((cols - 0.5f) / cols, (rows - 0.5f) / rows);
-	qglVertex2f(x + w, y + h);
-	qglTexCoord2f(0.5f / cols, (rows - 0.5f) / rows);
-	qglVertex2f(x, y + h);
-	qglEnd();
+	{
+		vec4_t quadVerts[4];
+		vec2_t texCoords[4];
+
+		Vector4Set(quadVerts[0], x, y, 0.0f, 1.0f);
+		Vector4Set(quadVerts[1], x + w, y, 0.0f, 1.0f);
+		Vector4Set(quadVerts[2], x + w, y + h, 0.0f, 1.0f);
+		Vector4Set(quadVerts[3], x, y + h, 0.0f, 1.0f);
+
+		Vector2Set(texCoords[0], 0.5f / cols, 0.5f / rows);
+		Vector2Set(texCoords[1], (cols - 0.5f) / cols, 0.5f / rows);
+		Vector2Set(texCoords[2], (cols - 0.5f) / cols, (rows - 0.5f) / rows);
+		Vector2Set(texCoords[3], 0.5f / cols, (rows - 0.5f) / rows);
+
+		RB_InstantQuad2(quadVerts, texCoords);
+	}
 }
 
 void RE_UploadCinematic(int w, int h, int cols, int rows, const byte *data, int client, qboolean dirty) {
@@ -883,6 +929,7 @@ void RB_ShowImages(void) {
 	image_t *image;
 	float x, y, w, h;
 	int start, end;
+	vec4_t quadVerts[4];
 
 	if (!backEnd.projection2D) {
 		RB_SetGL2D();
@@ -909,16 +956,13 @@ void RB_ShowImages(void) {
 		}
 
 		GL_Bind(image);
-		qglBegin(GL_QUADS);
-		qglTexCoord2f(0, 0);
-		qglVertex2f(x, y);
-		qglTexCoord2f(1, 0);
-		qglVertex2f(x + w, y);
-		qglTexCoord2f(1, 1);
-		qglVertex2f(x + w, y + h);
-		qglTexCoord2f(0, 1);
-		qglVertex2f(x, y + h);
-		qglEnd();
+
+		Vector4Set(quadVerts[0], x, y, 0, 1);
+		Vector4Set(quadVerts[1], x + w, y, 0, 1);
+		Vector4Set(quadVerts[2], x + w, y + h, 0, 1);
+		Vector4Set(quadVerts[3], x, y + h, 0, 1);
+
+		RB_InstantQuad(quadVerts);
 	}
 
 	qglFinish();
