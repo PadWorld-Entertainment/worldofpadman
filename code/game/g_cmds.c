aedbd00953e1e5cb46ed7805bfbb0281ec7dea28
Zack Middleton <zack@cloemail.com>
2019-11-16 00:57:17 -0600

    Fix duplicate team join center print for bots and g_teamAutoJoin
    My 2017 commit f7c3276 ("Fix g_teamAutoJoin and g_teamForceBalance")
    caused BroadcastTeamChange() to be run an extra time when bots connect.
    This caused a duplicate center print message to be sent to all clients.
    
    g_teamAutoJoin 1 sent an extra center print when any player or bot
    connects. Though that is the original behavior from Team Arena.
    
    Team set during connection no longer sends broadcast as it will be
    sent later in ClientConnect().
    
    Reported by Pascal Brochart (pbrochart) as causing clients to be
    kicked due to server command overflow if many bots join at the
    same time.

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -656,8 +656,6 @@ void SetTeam(gentity_t *ent, const char *s) {
 		CheckTeamLeader(oldTeam);
 	}
 
-	BroadcastTeamChange(client, oldTeam);
-
 	// get and distribute relevant parameters
 	ClientUserinfoChanged(clientNum);
 
@@ -665,6 +663,8 @@ void SetTeam(gentity_t *ent, const char *s) {
 	if (client->pers.connected != CON_CONNECTED) {
 		return;
 	}
+
+	BroadcastTeamChange(client, oldTeam);
 
 	ClientBegin(clientNum);
 }
