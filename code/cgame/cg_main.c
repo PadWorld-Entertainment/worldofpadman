a07ea325c9803c403198054f6e3ab986c523ce1f
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-04-01 16:19:21 +0200

    CGAME: use trap_Print
    see 2db73231ebd237c0b3a6eb7b249479906a50bf02 in ioq3
    
    (cherry picked from commit d0445f96c8109ceb01054b5e79a931720874c7b2)

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -586,7 +586,7 @@ void QDECL Com_Error( int level, const char *error, ... ) {
 	Q_vsnprintf (text, sizeof(text), error, argptr);
 	va_end (argptr);
 
-	CG_Error( "%s", text);
+	trap_Error( text );
 }
 
 void QDECL Com_Printf( const char *msg, ... ) {
@@ -597,7 +597,7 @@ void QDECL Com_Printf( const char *msg, ... ) {
 	Q_vsnprintf (text, sizeof(text), msg, argptr);
 	va_end (argptr);
 
-	CG_Printf ("%s", text);
+	trap_Print( text );
 }
 
 #endif
