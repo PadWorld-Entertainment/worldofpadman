8193e4c50a78840d3fc0a381579a56ea75b08c2f
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-06-02 20:22:04 +0200

    MISC: quake3 => wop

--- misc/linux/start_server.sh
+++ misc/linux/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "Edit this script to change the path to ioquake3's dedicated server executable and which binary if you aren't on x86_64."
-echo "Set the sv_dlURL setting to a url like http://yoursite.com/ioquake3_path for ioquake3 clients to download extra data."
+echo "Edit this script to change the path to wop's dedicated server executable and which binary if you aren't on x86_64."
+echo "Set the sv_dlURL setting to a url like http://yoursite.com/wop_path for wop clients to download extra data."
 
-# sv_dlURL needs to have quotes escaped like \"http://yoursite.com/ioquake3_path\" or it will be set to "http:" in-game.
+# sv_dlURL needs to have quotes escaped like \"http://yoursite.com/wop_path\" or it will be set to "http:" in-game.
 
-~/ioquake3/ioq3ded.x86_64 +set dedicated 2 +set sv_allowDownload 1 +set sv_dlURL \"\" +set com_hunkmegs 64 "$@"
+~/worldofpadman/wopded.x86_64 +set dedicated 2 +set sv_allowDownload 1 +set sv_dlURL \"\" +set com_hunkmegs 64 "$@"
