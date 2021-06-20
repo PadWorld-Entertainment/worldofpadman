82781e6947bb520454fbaddb88cd34a0d05e75a8
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-06-20 20:09:51 +0200

    RENDERER: static

--- code/renderer_vulkan/tr_marks.c
+++ code/renderer_vulkan/tr_marks.c
@@ -131,7 +131,7 @@ R_BoxSurfaces_r
 
 =================
 */
-void R_BoxSurfaces_r(mnode_t *node, vec3_t mins, vec3_t maxs, surfaceType_t **list, int listsize, int *listlength,
+static void R_BoxSurfaces_r(mnode_t *node, vec3_t mins, vec3_t maxs, surfaceType_t **list, int listsize, int *listlength,
 					 vec3_t dir) {
 
 	int s, c;
@@ -193,7 +193,7 @@ R_AddMarkFragments
 
 =================
 */
-void R_AddMarkFragments(int numClipPoints, vec3_t clipPoints[2][MAX_VERTS_ON_POLY], int numPlanes, vec3_t *normals,
+static void R_AddMarkFragments(int numClipPoints, vec3_t clipPoints[2][MAX_VERTS_ON_POLY], int numPlanes, vec3_t *normals,
 						float *dists, int maxPoints, vec3_t pointBuffer, int maxFragments,
 						markFragment_t *fragmentBuffer, int *returnedPoints, int *returnedFragments, vec3_t mins,
 						vec3_t maxs) {
