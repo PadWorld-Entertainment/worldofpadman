5c012a333701c0d47cb9660a61e8a73f325f304b
Kai-Li <kai-li@worldofpadman.net>
2021-07-01 12:21:16 +0200

    CG: Moving spraypuff / spraymark to weaponsfx

--- code/cgame/cg_spraylogo.c
+++ code/cgame/cg_spraylogo.c
@@ -218,8 +218,8 @@ The main-init for the spraylogosys ... should be called at every vid_restart
 #######################
 */
 void Init_SprayLogoSys(void) {
-	cgs.media.spraypuff = trap_R_RegisterShader("weaponeffect/spraypuff");
-	cgs.media.spraymark = trap_R_RegisterShader("weaponeffect/spraymark");
+	cgs.media.spraypuff = trap_R_RegisterShader("models/weaponsfx/spraypuff");
+	cgs.media.spraymark = trap_R_RegisterShader("models/weaponsfx/spraymark");
 	cgs.media.slmenu_arrowr = trap_R_RegisterShaderNoMip("menu/arrows/headblu_rt0");
 	cgs.media.slmenu_arrowl = trap_R_RegisterShaderNoMip("menu/arrows/headblu_lt0");
 	cgs.media.cgwopmenu_cursor = trap_R_RegisterShaderNoMip("menu/art/3_cursor2");
