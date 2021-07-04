7f810eb371dcfe5f0614cd05e31f3409af947a61
Kai-Li <kai-li@worldofpadman.net>
2021-07-04 21:22:22 +0200

    CG: Path adjustment for new column header graphic for score/lives

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -942,7 +942,7 @@ static void CG_RegisterGraphics(void) {
 
 	if (cgs.gametype == GT_LPS || cg_buildScript.integer) {
 		cgs.media.scoreboardlivesleft = trap_R_RegisterShaderNoMip("menu/tab/lives");
-		cgs.media.scoreboardscore_lives = trap_R_RegisterShaderNoMip("scoreboard/score_lives");
+		cgs.media.scoreboardscore_lives = trap_R_RegisterShaderNoMip("menu/tab/score_lives");
 
 		cgs.media.lpsIcon = trap_R_RegisterShaderNoMip("icons/LPSwallhackicon");
 		cgs.media.lpsIconLead = trap_R_RegisterShaderNoMip("icons/LPSwallhackleadicon");
