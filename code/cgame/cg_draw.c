e1be910477665d7fb78bd2b0805e6b67cdf1e5b7
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-10-30 19:31:48 +0200

    Revert "CGAME: enable temporary printing of remaining floater seconds"
    This reverts commit 7888dceec7d613525612491ee485bc0db2fedf90.

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -1262,7 +1262,7 @@ static void CG_DrawHoldableItem(float y) {
 			CG_FillRect(barX, y + ICON_SIZE - barHeight, 10, barHeight, barColor);
 			CG_DrawRect(barX, y, 10, ICON_SIZE, 1.0f, colorWhite);
 		}
-		if (itemId == HI_KILLERDUCKS || itemId == HI_BOOMIES || itemId == HI_FLOATER) {
+		if (itemId == HI_KILLERDUCKS || itemId == HI_BOOMIES) {
 			const char *str = va("%i", itemState);
 			const int maxChars = strlen(str);
 			CG_DrawStringExt(640 - 24 - maxChars * 4, y + 8, str, colorWhite, qtrue, qtrue,
