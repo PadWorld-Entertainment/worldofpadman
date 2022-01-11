cad0966aec75059b6ff31789976f939f1ce00f87
Kai-Li <kai-li@worldofpadman.net>
2022-01-11 22:02:14 +0100

    Adjustments for demos menu to wscreen
    - also renamed demo2 to demos
    - replaced menubitmap1024s_s by menubitmap_s
    - demo list is now center aligned, like the mods list

--- code/ui/ui_demos.c
+++ code/ui/ui_demos.c
@@ -30,33 +30,32 @@ DEMOS MENU
 
 #include "ui_local.h"
 
-#define BACK0 "menu/buttons/back0"
-#define BACK1 "menu/buttons/back1"
-#define GO0 "menu/buttons/play_yel0"
-#define GO1 "menu/buttons/play_yel1"
 #define ARROWUP0 "menu/arrows/headblu_up0"
 #define ARROWUP1 "menu/arrows/headblu_up1"
 #define ARROWDN0 "menu/arrows/headblu_dn0"
 #define ARROWDN1 "menu/arrows/headblu_dn1"
+#define BACK0 "menu/buttons/back0"
+#define BACK1 "menu/buttons/back1"
+#define GO0 "menu/buttons/play_yel0"
+#define GO1 "menu/buttons/play_yel1"
 
 #define MAX_DEMOS 1024
 #define NAMEBUFSIZE (MAX_DEMOS * 32)
 
-#define ID_BACK 10
-#define ID_GO 11
-#define ID_LIST 12
-#define ID_SCROLL_UP 13
-#define ID_SCROLL_DOWN 14
+#define ID_LIST 10
+#define ID_SCROLL_UP 11
+#define ID_SCROLL_DOWN 12
+#define ID_BACK 13
+#define ID_GO 14
 
 typedef struct {
 	menuframework_s menu;
 
 	menulist_s list;
-
-	menubitmap1024s_s left;
-	menubitmap1024s_s right;
+	menubitmap_s up;
+	menubitmap_s down;
+	menubitmap_s go;
 	menubitmap_s back;
-	menubitmap1024s_s go;
 
 	char names[NAMEBUFSIZE];
 	char *demolist[MAX_DEMOS];
@@ -66,10 +65,10 @@ static demos_t s_demos;
 
 /*
 ===============
-Demos_MenuEvent
+UI_Demos_MenuEvent
 ===============
 */
-static void Demos_MenuEvent(void *ptr, int event) {
+static void UI_Demos_MenuEvent(void *ptr, int event) {
 	if (event != QM_ACTIVATED) {
 		return;
 	}
@@ -145,56 +144,62 @@ static void Demos_MenuInit(void) {
 	s_demos.menu.wrapAround = qtrue;
 	s_demos.menu.bgparts = BGP_DEMOS | BGP_MENUFX;
 
-	s_demos.left.generic.type = MTYPE_BITMAP1024S;
-	s_demos.left.x = 96;  // 367;
-	s_demos.left.y = 240; // 524;
-	s_demos.left.w = 38;  // 99;
-	s_demos.left.h = 98;  // 38;
-	s_demos.left.shader = trap_R_RegisterShaderNoMip(ARROWUP0);
-	s_demos.left.mouseovershader = trap_R_RegisterShaderNoMip(ARROWUP1);
-	s_demos.left.generic.callback = Demos_MenuEvent;
-	s_demos.left.generic.id = ID_SCROLL_DOWN;
+	s_demos.up.generic.type = MTYPE_BITMAP;
+	s_demos.up.generic.name = ARROWUP0;
+	s_demos.up.generic.flags = QMF_LEFT_JUSTIFY | QMF_HIGHLIGHT_IF_FOCUS;
+	s_demos.up.generic.callback = UI_Demos_MenuEvent;
+	s_demos.up.generic.id = ID_SCROLL_UP;
+	s_demos.up.generic.x = 62;
+	s_demos.up.generic.y = 140;
+	s_demos.up.width = 26;
+	s_demos.up.height = 60;
+	s_demos.up.focuspic = ARROWUP1;
+	s_demos.up.focuspicinstead = qtrue;
 
-	s_demos.right.generic.type = MTYPE_BITMAP1024S;
-	s_demos.right.x = 96;  // 561;
-	s_demos.right.y = 422; // 524;
-	s_demos.right.w = 38;  // 98;
-	s_demos.right.h = 98;  // 38;
-	s_demos.right.shader = trap_R_RegisterShaderNoMip(ARROWDN0);
-	s_demos.right.mouseovershader = trap_R_RegisterShaderNoMip(ARROWDN1);
-	s_demos.right.generic.callback = Demos_MenuEvent;
-	s_demos.right.generic.id = ID_SCROLL_UP;
+	s_demos.down.generic.type = MTYPE_BITMAP;
+	s_demos.down.generic.name = ARROWDN0;
+	s_demos.down.generic.flags = QMF_LEFT_JUSTIFY | QMF_HIGHLIGHT_IF_FOCUS;
+	s_demos.down.generic.callback = UI_Demos_MenuEvent;
+	s_demos.down.generic.id = ID_SCROLL_DOWN;
+	s_demos.down.generic.x = 62;
+	s_demos.down.generic.y = 256;
+	s_demos.down.width = 26;
+	s_demos.down.height = 60;
+	s_demos.down.focuspic = ARROWDN1;
+	s_demos.down.focuspicinstead = qtrue;
+
+	s_demos.go.generic.type = MTYPE_BITMAP;
+	s_demos.go.generic.name = GO0;
+	s_demos.go.generic.flags = QMF_LEFT_JUSTIFY | QMF_HIGHLIGHT_IF_FOCUS;
+	s_demos.go.generic.callback = UI_Demos_MenuEvent;
+	s_demos.go.generic.id = ID_GO;
+	s_demos.go.generic.x = 55;
+	s_demos.go.generic.y = 208;
+	s_demos.go.width = 40;
+	s_demos.go.height = 40;
+	s_demos.go.focuspic = GO1;
+	s_demos.go.focuspicinstead = qtrue;
 
 	s_demos.back.generic.type = MTYPE_BITMAP;
 	s_demos.back.generic.name = BACK0;
 	s_demos.back.generic.flags = QMF_LEFT_JUSTIFY | QMF_PULSEIFFOCUS;
 	s_demos.back.generic.x = 8;
-	s_demos.back.generic.y = 440;
+	s_demos.back.generic.y = 446;
 	s_demos.back.generic.id = ID_BACK;
-	s_demos.back.generic.callback = Demos_MenuEvent;
+	s_demos.back.generic.callback = UI_Demos_MenuEvent;
 	s_demos.back.width = 80;
 	s_demos.back.height = 40;
 	s_demos.back.focuspic = BACK1;
 	s_demos.back.focuspicinstead = qtrue;
 
-	s_demos.go.generic.type = MTYPE_BITMAP1024S;
-	s_demos.go.x = 84;	// 815;
-	s_demos.go.y = 350; // 633;
-	s_demos.go.w = 63;	// 181;
-	s_demos.go.h = 63;	// 110;
-	s_demos.go.shader = trap_R_RegisterShaderNoMip(GO0);
-	s_demos.go.mouseovershader = trap_R_RegisterShaderNoMip(GO1);
-	s_demos.go.generic.callback = Demos_MenuEvent;
-	s_demos.go.generic.id = ID_GO;
-
 	s_demos.list.generic.type = MTYPE_SCROLLLIST;
-	s_demos.list.generic.flags = QMF_PULSEIFFOCUS;
-	s_demos.list.generic.callback = Demos_MenuEvent;
+	s_demos.list.generic.flags = QMF_PULSEIFFOCUS | QMF_CENTER_JUSTIFY;
+	s_demos.list.generic.callback = UI_Demos_MenuEvent;
 	s_demos.list.generic.id = ID_LIST;
-	s_demos.list.generic.x = 100;
+	s_demos.list.generic.x = 252;
 	s_demos.list.generic.y = 60;
-	s_demos.list.width = 28;
-	s_demos.list.height = 20;
+	s_demos.list.width = 37;
+	s_demos.list.height = 21;
 	s_demos.list.columns = 1;
 
 	demoname = s_demos.names;
@@ -233,8 +238,8 @@ static void Demos_MenuInit(void) {
 	qsort(s_demos.list.itemnames, s_demos.list.numitems, sizeof(s_demos.list.itemnames[0]), UI_SortDemos);
 
 	Menu_AddItem(&s_demos.menu, &s_demos.list);
-	Menu_AddItem(&s_demos.menu, &s_demos.left);
-	Menu_AddItem(&s_demos.menu, &s_demos.right);
+	Menu_AddItem(&s_demos.menu, &s_demos.up);
+	Menu_AddItem(&s_demos.menu, &s_demos.down);
 	Menu_AddItem(&s_demos.menu, &s_demos.go);
 	Menu_AddItem(&s_demos.menu, &s_demos.back);
 }
@@ -245,10 +250,14 @@ Demos_Cache
 =================
 */
 void Demos_Cache(void) {
-	trap_R_RegisterShaderNoMip(BACK0);
-	trap_R_RegisterShaderNoMip(BACK1);
+	trap_R_RegisterShaderNoMip(ARROWUP0);
+	trap_R_RegisterShaderNoMip(ARROWUP1);
+	trap_R_RegisterShaderNoMip(ARROWDN0);
+	trap_R_RegisterShaderNoMip(ARROWDN1);
 	trap_R_RegisterShaderNoMip(GO0);
 	trap_R_RegisterShaderNoMip(GO1);
+	trap_R_RegisterShaderNoMip(BACK0);
+	trap_R_RegisterShaderNoMip(BACK1);
 }
 
 /*
