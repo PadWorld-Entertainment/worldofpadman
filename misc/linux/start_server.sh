56a2a009ecca79ed95c74560a0a1612657bfffea
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-01-24 18:29:23 +0100

    MISC: removed unused stuff and integrate some installer files into cmake

--- misc/linux/start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Edit this script to change the path to wop's dedicated server executable and which binary if you aren't on x86_64."
-echo "Set the sv_dlURL setting to a url like http://yoursite.com/wop_path for wop clients to download extra data."
-
-# sv_dlURL needs to have quotes escaped like \"http://yoursite.com/wop_path\" or it will be set to "http:" in-game.
-
-~/worldofpadman/wopded.x86_64 +set dedicated 2 +set sv_allowDownload 1 +set sv_dlURL \"\" +set com_hunkmegs 64 "$@"
