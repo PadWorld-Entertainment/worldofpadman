5603a46fb24625219ceff2d8cfc1298621af53dc
Kai Bergmann <44040989+kai-li-wop@users.noreply.github.com>
2022-03-22 10:45:34 +0100

    UI: Should fix #138 without reopening #87
    This should enable the user to re-chose the logo via the player settings menu while being in a match AND avoiding the 'unknown command selectlogo' message in the player setting menu while not being in a match.

--- code/ui/ui_playersettings.c
+++ code/ui/ui_playersettings.c
@@ -259,7 +259,10 @@ static void PlayerSettings_SaveChanges(void) {
 	trap_Cvar_SetValue("color1", uitogamecode[s_playersettings.effects.curvalue]);
 
 	trap_Cvar_Set("syc_logo", uis.spraylogoNames[s_playersettings.slogo_num]);
-	trap_Cmd_ExecuteText( EXEC_APPEND, va( "selectlogo \"%s\"\n", uis.spraylogoNames[s_playersettings.slogo_num] ) );
+
+	if (trap_Cvar_VariableValue("cl_paused")) {
+		trap_Cmd_ExecuteText( EXEC_APPEND, va( "selectlogo \"%s\"\n", uis.spraylogoNames[s_playersettings.slogo_num] ) );
+	}
 }
 
 /*
