d5e33acf0e0cfd1fbc0d4164875c685d16882039
Kai Bergmann <44040989+kai-li-wop@users.noreply.github.com>
2022-03-30 13:52:47 +0200

    UI: Renaming Mouse Speed to Mouse Sensitivity

--- code/ui/ui_controls2.c
+++ code/ui/ui_controls2.c
@@ -1279,7 +1279,7 @@ static void Controls_MenuInit(void) {
 	s_controls.sensitivity.generic.type = MTYPE_SLIDER;
 	s_controls.sensitivity.generic.x = SCREEN_WIDTH / 2;
 	s_controls.sensitivity.generic.flags = QMF_SMALLFONT;
-	s_controls.sensitivity.generic.name = "Mouse Speed";
+	s_controls.sensitivity.generic.name = "Mouse Sensitivity";
 	s_controls.sensitivity.generic.id = ID_MOUSESPEED;
 	s_controls.sensitivity.generic.callback = Controls_MenuEvent;
 	s_controls.sensitivity.minvalue = 2;
