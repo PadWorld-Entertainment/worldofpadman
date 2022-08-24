cbcdb08b8ffc1a45790eb22e63b9387912d3fbdf
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-08-24 23:27:01 +0200

    JANITORIAL: idppc_altivec is set to 0 or 1 - so it's defined

--- code/renderergl1/tr_local.h
+++ code/renderergl1/tr_local.h
@@ -1546,7 +1546,7 @@ void RE_TakeVideoFrame(int width, int height, byte *captureBuffer, byte *encodeB
 void R_DrawElements(int numIndexes, const glIndex_t *indexes);
 void VectorArrayNormalize(vec4_t *normals, unsigned int count);
 
-#ifdef idppc_altivec
+#if idppc_altivec
 void LerpMeshVertexes_altivec(md3Surface_t *surf, float backlerp);
 void ProjectDlightTexture_altivec(void);
 void RB_CalcDiffuseColor_altivec(unsigned char *colors);
