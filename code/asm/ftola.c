614c793d44082e74c495e88c7bccce01e3c049a2
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-01-08 19:09:51 +0100

    ASM: fixed compiler warnings

--- code/asm/ftola.c
+++ code/asm/ftola.c
@@ -21,14 +21,12 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "qasm-inline.h"
+#include "../qcommon/q_shared.h"
 
-#if defined(__i386__) || defined(__x86_64__)
-static const unsigned short fpucw = 0x0C7F;
-
+#if idx64
 /*
  * GNU inline asm ftol conversion functions using SSE or FPU
  */
-
 long qftolsse(float f) {
 	long retval;
 
@@ -48,6 +46,9 @@ int qvmftolsse(void) {
 
 	return retval;
 }
+
+#elif id386
+static const unsigned short fpucw = 0x0C7F;
 
 long qftolx87(float f) {
 	long retval;
