e635fa0eb07795fdaf2aae8ef39c2bed763bb514
Kai-Li <kai-li@worldofpadman.net>
2023-02-03 16:03:03 +0100

    CGAME: Remove redundant reward sfx handles

--- code/cgame/cg_playerstate.c
+++ code/cgame/cg_playerstate.c
@@ -284,36 +284,31 @@ static void CG_CheckLocalSounds(playerState_t *ps, playerState_t *ops) {
 
 	// reward sounds
 	if (ps->persistant[PERS_EXCELLENT_COUNT] != ops->persistant[PERS_EXCELLENT_COUNT]) {
-		sfxHandle_t sfx = cgs.media.excellentSound;
-		CG_PushReward(sfx, cgs.media.medalExcellent, ps->persistant[PERS_EXCELLENT_COUNT]);
+		CG_PushReward(cgs.media.excellentSound, cgs.media.medalExcellent, ps->persistant[PERS_EXCELLENT_COUNT]);
 		reward = qtrue;
 		// Com_Printf("Excellent award\n");
 	}
 	if ((ps->persistant[PERS_SPRAYAWARDS_COUNT] & 0xFF00) != (ops->persistant[PERS_SPRAYAWARDS_COUNT] & 0xFF00)) {
-		sfxHandle_t sfx = cgs.media.spraygodSound;
-		CG_PushReward(sfx, cgs.media.medalSprayGod, (ps->persistant[PERS_SPRAYAWARDS_COUNT] >> 8));
+		CG_PushReward(cgs.media.spraygodSound, cgs.media.medalSprayGod, (ps->persistant[PERS_SPRAYAWARDS_COUNT] >> 8));
 		reward = qtrue;
 		// Com_Printf("SprayGod award\n");
 	}
 	if ((ps->persistant[PERS_SPRAYAWARDS_COUNT] & 0xFF) != (ops->persistant[PERS_SPRAYAWARDS_COUNT] & 0xFF)) {
-		sfxHandle_t sfx = cgs.media.spraykillerSound;
-		CG_PushReward(sfx, cgs.media.medalSprayKiller, ps->persistant[PERS_SPRAYAWARDS_COUNT] & 0xFF);
+		CG_PushReward(cgs.media.spraykillerSound, cgs.media.medalSprayKiller, ps->persistant[PERS_SPRAYAWARDS_COUNT] & 0xFF);
 		reward = qtrue;
 		// Com_Printf("SprayKiller award\n");
 	}
 	if (ps->persistant[PERS_CAPTURES] != ops->persistant[PERS_CAPTURES]) {
-		sfxHandle_t sfx = cgs.media.padstarSound;
 		if (cgs.clientinfo[ps->clientNum].team == TEAM_RED) {
-			CG_PushReward(sfx, cgs.media.medalPadStarRed, ps->persistant[PERS_CAPTURES]);			
+			CG_PushReward(cgs.media.padstarSound, cgs.media.medalPadStarRed, ps->persistant[PERS_CAPTURES]);			
 		} else {
-			CG_PushReward(sfx, cgs.media.medalPadStar, ps->persistant[PERS_CAPTURES]);
+			CG_PushReward(cgs.media.padstarSound, cgs.media.medalPadStar, ps->persistant[PERS_CAPTURES]);
 		}
 		reward = qtrue;
 		// Com_Printf("PadStar award\n");
 	}
 	if (ps->persistant[PERS_SNACKATTACK_COUNT] != ops->persistant[PERS_SNACKATTACK_COUNT]) {
-		sfxHandle_t sfx = cgs.media.snackattackSound;
-		CG_PushReward(sfx, cgs.media.medalSnackAttack, ps->persistant[PERS_SNACKATTACK_COUNT]);
+		CG_PushReward(cgs.media.snackattackSound, cgs.media.medalSnackAttack, ps->persistant[PERS_SNACKATTACK_COUNT]);
 		reward = qtrue;
 		// Com_Printf("SnackAttack award\n");
 	}
