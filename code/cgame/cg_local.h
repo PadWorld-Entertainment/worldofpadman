PadWorld-Entertainment/worldofpadman

commit 010314ab264a07cffc1173de0fcd6fd307adfa99

WOP: Reorganization of sound files (step 6)
Transfer of game type sounds from /sounds to /sound, except for ctl. A little resorting and renaming of files and handles.
- reestablished "blue/red scores" sounds. we used here "blue/red rules" of Angie instead because people did not liked the "blue/red scores" by Eileen. Angie will hopefully provide us a suitable version of "blue/red scores" soon.
- LPS features "better luck next time" as additional lose sound now. Would be nice to see those win and lose sounds also in FFA. Proper events are missing, see comment in cg_scoreboard.c line 197 (FIXME: These should be events caused by game)

code/cgame/cg_local.h
@@ -991,8 +991,8 @@ typedef struct {
 	sfxHandle_t BalloonyExplosion;
 	sfxHandle_t BettyExplosion;
 	//:HERBY:ea
-	sfxHandle_t ballonAufblasSound;
-	sfxHandle_t ballonPlatztSound;
+	sfxHandle_t balloonInflateSound;
+	sfxHandle_t balloonBurstSound;
 	//:HERBY:ee
 
 	// sounds
@@ -1021,15 +1021,15 @@ typedef struct {
 	sfxHandle_t tenSecondsToLeaveSound;
 
 	sfxHandle_t welcomeSound[3];
-	sfxHandle_t Announcer_SRfrag[2];
-	sfxHandle_t Announcer_SprayYourColor;
+	sfxHandle_t byeSprayRoomSound[2];
+	sfxHandle_t sprayYourColorSound;
 
 	//:HERBY:ea
-	sfxHandle_t Announcer_RedBalloon;
-	sfxHandle_t Announcer_BlueBalloon;
-	sfxHandle_t Announcer_BigBalloonRed;
-	sfxHandle_t Announcer_BigBalloonBlue;
-	sfxHandle_t Announcer_BalloonDestroyed;
+	sfxHandle_t redBalloonSound;
+	sfxHandle_t blueBalloonSound;
+	sfxHandle_t redBigBalloonSound;
+	sfxHandle_t blueBigBalloonSound;
+	sfxHandle_t balloonDestroyedSound;
 	//:HERBY:ee
 
 	sfxHandle_t oneLogoLeft;
@@ -1038,8 +1038,8 @@ typedef struct {
 
 	sfxHandle_t wrongSprayWallSound[4];
 	sfxHandle_t loseFFASound;
-	sfxHandle_t winLPSSounds[2];
-	sfxHandle_t YouLooseSound; // for lps round-loose
+	sfxHandle_t youWinSound[2];
+	sfxHandle_t youLoseSound[2];
 
 	sfxHandle_t suddenDeathSound;
 

code/cgame/cg_main.c
@@ -629,23 +629,12 @@ static void CG_RegisterSounds(void) {
 	cgs.media.count3Sound = trap_S_RegisterSound("sound/feedback/numbers/three", qtrue);
 	cgs.media.count2Sound = trap_S_RegisterSound("sound/feedback/numbers/two", qtrue);
 	cgs.media.count1Sound = trap_S_RegisterSound("sound/feedback/numbers/one", qtrue);
-	cgs.media.countFightSound = trap_S_RegisterSound("sound/feedback/make_your_game", qtrue);
-
+	cgs.media.countFightSound = trap_S_RegisterSound("sound/feedback/make_game", qtrue);
 	cgs.media.pickupSound = trap_S_RegisterSound("sounds/weapons/weapon_pickup", qfalse);
 	cgs.media.HIpickupSound = trap_S_RegisterSound("sound/items/holdable/pickup", qfalse);
 	cgs.media.ARpickupSound = trap_S_RegisterSound("sound/items/padshield/pickup", qfalse);
 
 	if ((cgs.gametype == GT_SPRAYFFA || cgs.gametype == GT_SPRAY) || cg_buildScript.integer) {
-		cgs.media.Announcer_SRfrag[0] = trap_S_RegisterSound("sounds/gametype/syc/nice_day", qtrue);
-		cgs.media.Announcer_SRfrag[1] = trap_S_RegisterSound("sounds/gametype/syc/goodbye", qtrue);
-
-		cgs.media.Announcer_SprayYourColor = trap_S_RegisterSound("sounds/gametype/syc/syc", qtrue);
-
-		cgs.media.wrongSprayWallSound[0] = trap_S_RegisterSound("sounds/gametype/syc/colorblind", qtrue);
-		cgs.media.wrongSprayWallSound[1] = trap_S_RegisterSound("sounds/gametype/syc/omg", qtrue);
-		cgs.media.wrongSprayWallSound[2] = trap_S_RegisterSound("sounds/gametype/syc/stupid_sprayer", qtrue);
-		cgs.media.wrongSprayWallSound[3] = trap_S_RegisterSound("sounds/gametype/syc/who_is", qtrue);
-
 		cgs.media.countSprayRoomSound[0] = cgs.media.count1Sound;
 		cgs.media.countSprayRoomSound[1] = cgs.media.count2Sound;
 		cgs.media.countSprayRoomSound[2] = cgs.media.count3Sound;
@@ -656,10 +645,19 @@ static void CG_RegisterSounds(void) {
 		cgs.media.countSprayRoomSound[7] = trap_S_RegisterSound("sound/feedback/numbers/eight", qtrue);
 		cgs.media.countSprayRoomSound[8] = trap_S_RegisterSound("sound/feedback/numbers/nine", qtrue);
 		cgs.media.countSprayRoomSound[9] = trap_S_RegisterSound("sound/feedback/numbers/ten", qtrue);
-		cgs.media.tenSecondsToLeaveSound = trap_S_RegisterSound("sound/feedback/countdown/10_seconds_to_leave", qtrue);
-
+		cgs.media.tenSecondsToLeaveSound = trap_S_RegisterSound("sound/feedback/ten_seconds", qtrue);
+		cgs.media.byeSprayRoomSound[0] = trap_S_RegisterSound("sound/feedback/nice_day", qtrue);
+		cgs.media.byeSprayRoomSound[1] = trap_S_RegisterSound("sound/feedback/goodbye", qtrue);
+		cgs.media.sprayYourColorSound = trap_S_RegisterSound("sound/feedback/spray_color", qtrue);
 		cgs.media.spraygodSound = trap_S_RegisterSound("sound/feedback/awards/spraygod", qtrue);
 		cgs.media.spraykillerSound = trap_S_RegisterSound("sound/feedback/awards/spraykiller", qtrue);
+	}
+
+	if (cgs.gametype == GT_SPRAY || cg_buildScript.integer) {
+		cgs.media.wrongSprayWallSound[0] = trap_S_RegisterSound("sound/feedback/teamplay/colorblind_sprayer", qtrue);
+		cgs.media.wrongSprayWallSound[1] = trap_S_RegisterSound("sound/feedback/teamplay/omg_spray", qtrue);
+		cgs.media.wrongSprayWallSound[2] = trap_S_RegisterSound("sound/feedback/teamplay/stupid_sprayer", qtrue);
+		cgs.media.wrongSprayWallSound[3] = trap_S_RegisterSound("sound/feedback/teamplay/who_is_stupid_sprayer", qtrue);
 	}
 
 	if (cgs.gametype == GT_SPRAYFFA || cg_buildScript.integer) {
@@ -669,11 +667,10 @@ static void CG_RegisterSounds(void) {
 	}
 
 	if (cgs.gametype < GT_TEAM || cg_buildScript.integer) {
-		cgs.media.loseFFASound = trap_S_RegisterSound("sounds/gametype/dm/better_luck", qtrue);
-
-		cgs.media.takenLeadSound = trap_S_RegisterSound("sounds/gametype/dm/lead_taken", qtrue);
-		cgs.media.tiedLeadSound = trap_S_RegisterSound("sounds/gametype/dm/lead_tied", qtrue);
-		cgs.media.lostLeadSound = trap_S_RegisterSound("sounds/gametype/dm/lead_lost", qtrue);
+		cgs.media.loseFFASound = trap_S_RegisterSound("sound/feedback/better_luck", qtrue);
+		cgs.media.takenLeadSound = trap_S_RegisterSound("sound/feedback/lead_taken", qtrue);
+		cgs.media.tiedLeadSound = trap_S_RegisterSound("sound/feedback/lead_tied", qtrue);
+		cgs.media.lostLeadSound = trap_S_RegisterSound("sound/feedback/lead_lost", qtrue);
 	}
 
 	if (cgs.gametype < GT_CTF || cg_buildScript.integer) {
@@ -685,34 +682,33 @@ static void CG_RegisterSounds(void) {
 	}
 
 	if (cgs.gametype == GT_LPS || cg_buildScript.integer) {
-		cgs.media.winLPSSounds[0] = trap_S_RegisterSound("sounds/gametype/lps/you_win", qtrue);
-		cgs.media.winLPSSounds[1] = trap_S_RegisterSound("sounds/gametype/lps/won_night", qtrue);
-		cgs.media.YouLooseSound = trap_S_RegisterSound("sounds/gametype/lps/you_lose", qtrue);
+		cgs.media.youWinSound[0] = trap_S_RegisterSound("sound/feedback/you_win", qtrue);
+		cgs.media.youWinSound[1] = trap_S_RegisterSound("sound/feedback/congratulations", qtrue);
+		cgs.media.youLoseSound[0] = trap_S_RegisterSound("sound/feedback/you_lose", qtrue);
+		cgs.media.youLoseSound[1] = trap_S_RegisterSound("sound/feedback/better_luck", qtrue);
 	}
 
 	if (cgs.gametype == GT_BALLOON || cg_buildScript.integer) {
-		cgs.media.Announcer_RedBalloon = trap_S_RegisterSound("sounds/gametype/bb/red_balloon", qtrue);
-		cgs.media.Announcer_BlueBalloon = trap_S_RegisterSound("sounds/gametype/bb/blue_balloon", qtrue);
-		cgs.media.Announcer_BigBalloonRed = trap_S_RegisterSound("sounds/gametype/bb/bigballoon_red", qtrue);
-		cgs.media.Announcer_BigBalloonBlue = trap_S_RegisterSound("sounds/gametype/bb/bigballoon_blue", qtrue);
-		cgs.media.Announcer_BalloonDestroyed = trap_S_RegisterSound("sounds/gametype/bb/balloon_destroyed", qtrue);
-
-		cgs.media.ballonAufblasSound = trap_S_RegisterSound("sounds/gametype/bb/balloon_inflate", qfalse);
-		cgs.media.ballonPlatztSound = trap_S_RegisterSound("sounds/gametype/bb/balloon_burst", qfalse);
+		cgs.media.redBalloonSound = trap_S_RegisterSound("sound/feedback/teamplay/red_balloon", qtrue);
+		cgs.media.blueBalloonSound = trap_S_RegisterSound("sound/feedback/teamplay/blue_balloon", qtrue);
+		cgs.media.redBigBalloonSound = trap_S_RegisterSound("sound/feedback/teamplay/red_bigballoon", qtrue);
+		cgs.media.blueBigBalloonSound = trap_S_RegisterSound("sound/feedback/teamplay/blue_bigballoon", qtrue);
+		cgs.media.balloonDestroyedSound = trap_S_RegisterSound("sound/feedback/teamplay/balloon_destroyed", qtrue);
+		cgs.media.balloonInflateSound = trap_S_RegisterSound("sound/feedback/teamplay/balloon_inflate", qfalse);
+		cgs.media.balloonBurstSound = trap_S_RegisterSound("sound/feedback/teamplay/balloon_burst", qfalse);
 	}
 
 	if (cgs.gametype >= GT_TEAM || cg_buildScript.integer) {
-		cgs.media.redLeadsSound[0] = trap_S_RegisterSound("sounds/gametype/team/red_leads", qtrue);
-		cgs.media.redLeadsSound[1] = trap_S_RegisterSound("sounds/gametype/team/red_rules", qtrue);
-		cgs.media.redLeadsSound[2] = trap_S_RegisterSound("sounds/gametype/team/red_best", qtrue);
-		cgs.media.blueLeadsSound[0] = trap_S_RegisterSound("sounds/gametype/team/blue_leads", qtrue);
-		cgs.media.blueLeadsSound[1] = trap_S_RegisterSound("sounds/gametype/team/blue_rules", qtrue);
-		cgs.media.blueLeadsSound[2] = trap_S_RegisterSound("sounds/gametype/team/blue_best", qtrue);
-		cgs.media.teamsTiedSound = trap_S_RegisterSound("sounds/gametype/team/teams_tied", qtrue);
-		cgs.media.hitTeamSound = trap_S_RegisterSound("sounds/gametype/team/hit_teammate", qtrue);
-
-		cgs.media.redScoredSound = trap_S_RegisterSound("sounds/gametype/team/red_rules", qtrue);
-		cgs.media.blueScoredSound = trap_S_RegisterSound("sounds/gametype/team/blue_rules", qtrue);
+		cgs.media.redLeadsSound[0] = trap_S_RegisterSound("sound/feedback/teamplay/red_leads", qtrue);
+		cgs.media.redLeadsSound[1] = trap_S_RegisterSound("sound/feedback/teamplay/red_rules", qtrue);
+		cgs.media.redLeadsSound[2] = trap_S_RegisterSound("sound/feedback/teamplay/red_best", qtrue);
+		cgs.media.blueLeadsSound[0] = trap_S_RegisterSound("sound/feedback/teamplay/blue_leads", qtrue);
+		cgs.media.blueLeadsSound[1] = trap_S_RegisterSound("sound/feedback/teamplay/blue_rules", qtrue);
+		cgs.media.blueLeadsSound[2] = trap_S_RegisterSound("sound/feedback/teamplay/blue_best", qtrue);
+		cgs.media.teamsTiedSound = trap_S_RegisterSound("sound/feedback/teamplay/teams_tied", qtrue);
+		cgs.media.hitTeamSound = trap_S_RegisterSound("sound/feedback/hit_teammate", qtrue);
+		cgs.media.redScoredSound = trap_S_RegisterSound("sound/feedback/teamplay/red_scores", qtrue);
+		cgs.media.blueScoredSound = trap_S_RegisterSound("sound/feedback/teamplay/blue_scores", qtrue);
 
 		if (cgs.gametype == GT_CTF || cg_buildScript.integer) {
 			cgs.media.captureYourTeamSound = trap_S_RegisterSound("sounds/gametype/ctl/capture_team", qtrue);

code/cgame/cg_scoreboard.c
@@ -197,7 +197,7 @@ static void CG_DrawClientScore(int y, score_t *score, float *color, float fade, 
 			// FIXME: These should be events caused by game
 			if ((cg.snap->ps.pm_type == PM_INTERMISSION) && !cg.playedIntermissionMsg) {
 				if ((cgs.gametype == GT_LPS) && (rank == 0)) {
-					trap_S_StartLocalSound(cgs.media.winLPSSounds[(int)(random() * 1.9999f)], CHAN_ANNOUNCER);
+					trap_S_StartLocalSound(cgs.media.youWinSound[(int)(random() * 1.9999f)], CHAN_ANNOUNCER);
 				} else if (((rank + 1) == cg.scoreTeamCount[TEAM_FREE]) && (cg.scoreTeamCount[TEAM_FREE] > 1)) {
 					trap_S_StartLocalSound(cgs.media.loseFFASound, CHAN_ANNOUNCER);
 				}

code/cgame/cg_servercmds.c
@@ -255,9 +255,9 @@ void CG_UpdateBalloonStates(const char *str) {
 		if (team == '1' || team == '2') {
 			// announce
 			if (team == '1')
-				trap_S_StartLocalSound(cgs.media.Announcer_BigBalloonRed, CHAN_ANNOUNCER);
+				trap_S_StartLocalSound(cgs.media.redBigBalloonSound, CHAN_ANNOUNCER);
 			else if (team == '2')
-				trap_S_StartLocalSound(cgs.media.Announcer_BigBalloonBlue, CHAN_ANNOUNCER);
+				trap_S_StartLocalSound(cgs.media.blueBigBalloonSound, CHAN_ANNOUNCER);
 
 			// copy the new string
 			Q_strncpyz(cgs.balloonState, str, sizeof(cgs.balloonState));
@@ -269,13 +269,13 @@ void CG_UpdateBalloonStates(const char *str) {
 			if (str[i] != cgs.balloonState[i]) {
 				switch (str[i]) {
 				case '0':
-					CG_AddBufferedSound(cgs.media.Announcer_BalloonDestroyed);
+					CG_AddBufferedSound(cgs.media.balloonDestroyedSound);
 					break;
 				case '1':
-					CG_AddBufferedSound(cgs.media.Announcer_RedBalloon);
+					CG_AddBufferedSound(cgs.media.redBalloonSound);
 					break;
 				case '2':
-					CG_AddBufferedSound(cgs.media.Announcer_BlueBalloon);
+					CG_AddBufferedSound(cgs.media.blueBalloonSound);
 					break;
 
 				default:
@@ -653,7 +653,7 @@ static void CG_ServerCommand(void) {
 		switch (id) {
 		case CLIENT_DO_IT_SPRAYROOM_GOODBYE:
 			i = (int)(random() * 1.9999f);
-			trap_S_StartLocalSound(cgs.media.Announcer_SRfrag[i], CHAN_ANNOUNCER);
+			trap_S_StartLocalSound(cgs.media.byeSprayRoomSound[i], CHAN_ANNOUNCER);
 			break;
 
 		case CLIENT_DO_IT_SPRAYED_ON_WRONG_WALL:
@@ -662,8 +662,8 @@ static void CG_ServerCommand(void) {
 			break;
 
 		case CLIENT_DO_IT_LPS_LOST:
-			trap_S_StartLocalSound(cgs.media.YouLooseSound, CHAN_ANNOUNCER);
-			trap_S_StartLocalSound(cgs.media.YouLooseSound, CHAN_ANNOUNCER);
+			i = (int)(random() * 1.9999f);
+			trap_S_StartLocalSound(cgs.media.youLoseSound[i], CHAN_ANNOUNCER);
 			break;
 
 		// TODO: Remove!

code/cgame/cg_ents.c
@@ -993,7 +993,7 @@ static void CG_Balloon(centity_t *cent) {
 			s1->time = 4000;
 		if (s1->frame != cent->muzzleFlashTime) {
 			if (cent->lastweaponframe == 0)
-				trap_S_StartSound(s1->origin, s1->number, CHAN_AUTO, cgs.media.ballonAufblasSound);
+				trap_S_StartSound(s1->origin, s1->number, CHAN_AUTO, cgs.media.balloonInflateSound);
 			cent->lastweaponframe = cent->muzzleFlashTime;
 			cent->muzzleFlashTime = s1->frame;
 			cent->miscTime = cg.time;
@@ -1011,7 +1011,7 @@ static void CG_Balloon(centity_t *cent) {
 	} else {
 		// don't interpolate around frame 0
 		if (cent->muzzleFlashTime)
-			trap_S_StartSound(s1->origin, s1->number, CHAN_AUTO, cgs.media.ballonPlatztSound);
+			trap_S_StartSound(s1->origin, s1->number, CHAN_AUTO, cgs.media.balloonBurstSound);
 		cent->muzzleFlashTime = 0;
 	}
 

code/cgame/cg_view.c
@@ -1112,7 +1112,7 @@ void CG_DrawActiveFrame(int serverTime, stereoFrame_t stereoView, qboolean demoP
 	CG_PlayBufferedSounds();
 
 	if (cg.snap->ps.ammo[WP_SPRAYPISTOL] >= 8 && cg.sprayyourcolortime < cg.time) {
-		trap_S_StartLocalSound(cgs.media.Announcer_SprayYourColor, cg.snap->ps.clientNum);
+		trap_S_StartLocalSound(cgs.media.sprayYourColorSound, cg.snap->ps.clientNum);
 		cg.sprayyourcolortime = cg.time + 20000;
 	}
 

commit 72c0f09dbfe6beecdcdb5c06135e54629f97abd5

CGAME: Fixes "better Luck next time" played twice in LPS
This issue is not related to the "lives left" countdown feature, but was introduced with 010314ab264a07cffc1173de0fcd6fd307adfa99. In LPS, "you lose" is played when a player has lost all their lives, followed by "better luck next time" when the game/round ends and someone else has won. The "better luck" sound is triggered by a server command. Not being aware of this, I changed the "you lose" sound to randomly play either "you lose" or "better luck", resulting in "better luck" randomly playing twice in a row. Once when you lose your last life and the second time at the end of the match. If only a few starting lives are set (e.g. 2 or 1), this can quickly cause "better luck" to randomly play twice in quick succession. This fix undoes some of my previous changes and should restore original behavior. "You lose" should only be played when you lose your last life, and "More Luck" should only be played at the end of the match when someone else wins the match.

code/cgame/cg_local.h
@@ -1024,8 +1024,8 @@ typedef struct {
 
 	sfxHandle_t wrongSprayWallSound[4];
 	sfxHandle_t loseFFASound;
-	sfxHandle_t youWinSound[2];
-	sfxHandle_t youLoseSound[2];
+	sfxHandle_t loseLPSSound;
+	sfxHandle_t winLPSSound[2];
 
 	sfxHandle_t suddenDeathSound;
 

code/cgame/cg_main.c
@@ -686,10 +686,9 @@ static void CG_RegisterSounds(void) {
 		cgs.media.oneLifeSound = trap_S_RegisterSound("sound/feedback/count/1_life", qtrue);
 		cgs.media.twoLivesSound = trap_S_RegisterSound("sound/feedback/count/2_lives", qtrue);
 		cgs.media.threeLivesSound = trap_S_RegisterSound("sound/feedback/count/3_lives", qtrue);
-		cgs.media.youWinSound[0] = trap_S_RegisterSound("sound/feedback/you_win", qtrue);
-		cgs.media.youWinSound[1] = trap_S_RegisterSound("sound/feedback/congrats", qtrue);
-		cgs.media.youLoseSound[0] = trap_S_RegisterSound("sound/feedback/you_lose", qtrue);
-		cgs.media.youLoseSound[1] = trap_S_RegisterSound("sound/feedback/better_luck", qtrue);
+		cgs.media.winLPSSound[0] = trap_S_RegisterSound("sound/feedback/you_win", qtrue);
+		cgs.media.winLPSSound[1] = trap_S_RegisterSound("sound/feedback/congrats", qtrue);
+		cgs.media.loseLPSSound = trap_S_RegisterSound("sound/feedback/you_lose", qtrue);
 	}
 
 	if (cgs.gametype == GT_BALLOON || cg_buildScript.integer) {

code/cgame/cg_scoreboard.c
@@ -197,7 +197,7 @@ static void CG_DrawClientScore(int y, score_t *score, float *color, float fade, 
 			// FIXME: These should be events caused by game
 			if ((cg.snap->ps.pm_type == PM_INTERMISSION) && !cg.playedIntermissionMsg) {
 				if ((cgs.gametype == GT_LPS) && (rank == 0)) {
-					trap_S_StartLocalSound(cgs.media.youWinSound[(int)(random() * 1.9999f)], CHAN_ANNOUNCER);
+					trap_S_StartLocalSound(cgs.media.winLPSSound[(int)(random() * 1.9999f)], CHAN_ANNOUNCER);
 				} else if (((rank + 1) == cg.scoreTeamCount[TEAM_FREE]) && (cg.scoreTeamCount[TEAM_FREE] > 1)) {
 					trap_S_StartLocalSound(cgs.media.loseFFASound, CHAN_ANNOUNCER);
 				}

code/cgame/cg_servercmds.c
@@ -657,8 +657,7 @@ static void CG_ServerCommand(void) {
 			break;
 
 		case CLIENT_DO_IT_LPS_LOST:
-			i = (int)(random() * 1.9999f);
-			trap_S_StartLocalSound(cgs.media.youLoseSound[i], CHAN_ANNOUNCER);
+			trap_S_StartLocalSound(cgs.media.loseLPSSound, CHAN_ANNOUNCER);
 			break;
 
 		// TODO: Remove!