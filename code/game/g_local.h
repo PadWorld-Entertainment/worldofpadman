a14f3cbbbf309d2764e36a490c0c2293f9d9561f
Kai-Li <kai-li@worldofpadman.net>
2023-04-05 10:24:16 +0200

    GAME: Remove not used Team Arena function prototype

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -623,8 +623,6 @@ qboolean G_RadiusDamage(vec3_t origin, gentity_t *attacker, float damage, float 
 int G_InvulnerabilityEffect(gentity_t *targ, vec3_t dir, vec3_t point, vec3_t impactpoint, vec3_t bouncedir);
 void body_die(gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int damage, int meansOfDeath);
 void TossClientItems(gentity_t *self);
-void TossClientCubes(gentity_t *self);
-
 void BerserkerCheck(gentity_t *ent);
 
 // damage flags
