ioquake/ioq3

OpenGL2: Fix applying tcMod turb
Shader stage tcMods for matrix and turb effects need to be applied in
order for turb to be correct and all tcMod turb need to be applied
instead of only the last one.

Quake 3's textures/liquids/slime1 had tcMod turb and then tcMod scale.
OpenGL2 applied the matrix first and then turb which had the wrong result.

code/renderergl2/tr_glsl.c
@@ -88,8 +88,14 @@ static uniformInfo_t uniformsInfo[] =
 
 	{ "u_EnableTextures", GLSL_VEC4 },
 
-	{ "u_DiffuseTexMatrix",  GLSL_VEC4 },
-	{ "u_DiffuseTexOffTurb", GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix0",  GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix1",  GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix2",  GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix3",  GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix4",  GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix5",  GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix6",  GLSL_VEC4 },
+	{ "u_DiffuseTexMatrix7",  GLSL_VEC4 },
 
 	{ "u_TCGen0",        GLSL_INT },
 	{ "u_TCGen0Vector0", GLSL_VEC3 },

PadWorld-Entertainment/worldofpadman

code/renderergl2/tr_glsl.c
@@ -85,8 +85,14 @@ static uniformInfo_t uniformsInfo[] = {
 
 	{"u_EnableTextures", GLSL_VEC4},
 
-	{"u_DiffuseTexMatrix", GLSL_VEC4},
-	{"u_DiffuseTexOffTurb", GLSL_VEC4},
+	{"u_DiffuseTexMatrix0", GLSL_VEC4},
+	{"u_DiffuseTexMatrix1", GLSL_VEC4},
+	{"u_DiffuseTexMatrix2", GLSL_VEC4},
+	{"u_DiffuseTexMatrix3", GLSL_VEC4},
+	{"u_DiffuseTexMatrix4", GLSL_VEC4},
+	{"u_DiffuseTexMatrix5", GLSL_VEC4},
+	{"u_DiffuseTexMatrix6", GLSL_VEC4},
+	{"u_DiffuseTexMatrix7", GLSL_VEC4},
 
 	{"u_TCGen0", GLSL_INT},
 	{"u_TCGen0Vector0", GLSL_VEC3},