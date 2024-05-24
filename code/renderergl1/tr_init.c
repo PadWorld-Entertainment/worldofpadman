e491f518fa96469e5c95137eb949d4385a92ac52
Zack Middleton <zack@cloemail.com>
2024-05-24 15:16:16 -0500

    RENDERER: Remove packAlign from R_LevelShot()
    Filling the resample buffer didn't correctly handle offset for packAlign
    if it didn't match the alignment returned by Hunk_AllocateTempMemory().
    
    There isn't SIMD or something that the code wants a specific alignment.
    Hunk_AllocateTempMemory() is aligned to sizeof(intptr_t) anyway so there
    isn't a reason to add a memory alignment here.

--- code/renderergl1/tr_init.c
+++ code/renderergl1/tr_init.c
@@ -484,11 +484,10 @@ sampled down from full screen distorted images
 static void R_LevelShot(screenshotType_e type, const char *ext) {
 	char fileName[MAX_OSPATH];
 	byte *source, *allsource;
-	byte *resample, *resamplestart;
+	byte *resample;
 	size_t offset = 0, memcount;
-	int spadlen, rpadlen;
-	int padwidth, linelen;
-	GLint packAlign = 1;
+	int spadlen;
+	int linelen;
 	byte *src, *dst;
 	int x, y;
 	int r, g, b;
@@ -515,15 +514,9 @@ static void R_LevelShot(screenshotType_e type, const char *ext) {
 	source = allsource + offset;
 
 	linelen = width * 3;
-	padwidth = spadlen + linelen;
+	memcount = linelen * height;
 
-	// Allocate a few more bytes so that we can choose an alignment we like
-	resample = ri.Hunk_AllocateTempMemory(padwidth * height + offset + packAlign - 1);
-
-	resamplestart = PADP((intptr_t) resample + offset, packAlign);
-
-	offset = resamplestart - resample;
-	rpadlen = padwidth - linelen;
+	resample = ri.Hunk_AllocateTempMemory(memcount);
 
 	// resample from source
 	xScale = glConfig.vidWidth / (float)(width * 4.0f);
@@ -547,18 +540,16 @@ static void R_LevelShot(screenshotType_e type, const char *ext) {
 		}
 	}
 
-	memcount = (width * 3 + rpadlen) * height;
-
 	// gamma correct
 	if (glConfig.deviceSupportsGamma)
-		R_GammaCorrect(resample + offset, memcount);
+		R_GammaCorrect(resample, memcount);
 
 	if (type == ST_TGA)
-		RE_SaveTGA(fileName, width, height, resample + offset, rpadlen);
+		RE_SaveTGA(fileName, width, height, resample, 0);
 	else if (type == ST_JPEG)
-		RE_SaveJPG(fileName, r_screenshotJpegQuality->integer, width, height, resample + offset, rpadlen);
+		RE_SaveJPG(fileName, r_screenshotJpegQuality->integer, width, height, resample, 0);
 	else if (type == ST_PNG)
-		RE_SavePNG(fileName, width, height, resample + offset, rpadlen);
+		RE_SavePNG(fileName, width, height, resample, 0);
 
 	ri.Hunk_FreeTempMemory(resample);
 	ri.Hunk_FreeTempMemory(allsource);
