ad6c448804414db050ab6155dacb37e4f04a98b3
Simon McVittie <smcv@debian.org>
2025-08-23 10:54:32 +0100

    client: Remove i386 GNU assembler version of S_WriteLinearBlastStereo16()
    This was the last remaining GNU assembler code that was in its own
    independent source file, as opposed to being embedded in C code with
    GNU `__asm__ volatile` or MSVC `__asm` (which tends to be much easier
    to deal with in a modern build system).
    
    When the Quake 3 engine was originally written, this might have been
    an "expensive" function when written in portable C, but after 25 years
    of CPU development it's less likely to matter (and modern compilers
    might produce faster results from the C code anyway).
    
    Resolves: https://github.com/ioquake/ioq3/issues/778
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- code/qcommon/q_platform.h
+++ code/qcommon/q_platform.h
@@ -70,8 +70,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #endif
 
-#ifndef __ASM_I386__ // don't include the C bits if included from qasm.h
-
 // for windows fastcall option
 #define QDECL
 #define QCALL
@@ -406,8 +404,6 @@ float FloatSwap(const float *f);
 #define PLATFORM_STRING OS_STRING "-" ARCH_STRING
 #else
 #define PLATFORM_STRING OS_STRING "-" ARCH_STRING "-debug"
-#endif
-
 #endif
 
 #endif
