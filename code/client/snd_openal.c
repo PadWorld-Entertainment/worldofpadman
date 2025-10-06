65de3e4f600b1b01e42a459192cac21ebde153f5
Kai Bergmann <44040989+kai-li-wop@users.noreply.github.com>
2025-10-06 11:24:52 +0200

    CLIENT: Move missing sound warnings to developer console
    This should reduce the number of missing sound warnings in the console, which are useful only for developers. This should resolve issue #346.

--- code/client/snd_openal.c
+++ code/client/snd_openal.c
@@ -229,7 +229,7 @@ static void S_AL_BufferUseDefault(sfxHandle_t sfx) {
 	if (sfx == default_sfx)
 		Com_Error(ERR_FATAL, "Can't load default sound effect %s", knownSfx[sfx].filename);
 
-	Com_Printf(S_COLOR_YELLOW "WARNING: Using default sound for %s\n", knownSfx[sfx].filename);
+	Com_DPrintf(S_COLOR_YELLOW "WARNING: Using default sound for %s\n", knownSfx[sfx].filename);
 	knownSfx[sfx].isDefault = qtrue;
 	knownSfx[sfx].buffer = knownSfx[default_sfx].buffer;
 }
