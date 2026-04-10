463364dae42b6c2b49e92b7b5ade03f7c7a4146e
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-04-11 01:19:52 +0200

    VULKAN: fixed synchronization issue between the swapchain image being aquired and starting to write to it

--- code/renderer_vulkan/vk_frame.c
+++ code/renderer_vulkan/vk_frame.c
@@ -238,8 +238,17 @@ static void vk_createRenderPass(VkDevice device) {
 	// subpasses. Operations right before and right after this subpass also
 	// count as inplicit "subpasses".
 
-	desc.dependencyCount = 0;
-	desc.pDependencies = NULL;
+	VkSubpassDependency dependency;
+	dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+	dependency.dstSubpass = 0;
+	dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+	dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+	dependency.srcAccessMask = 0;
+	dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+	dependency.dependencyFlags = 0;
+
+	desc.dependencyCount = 1;
+	desc.pDependencies = &dependency;
 
 	VK_CHECK(qvkCreateRenderPass(device, &desc, NULL, &vk.render_pass));
 }
